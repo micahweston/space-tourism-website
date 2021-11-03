bcea025158282429f393e7802d17ff2639b33e42
Micah Weston <micah.weston@gmail.com>
2021-11-03 15:48:59 -0500

    finished crew and destination page started on technology page

--- tabs.js
+++ tabs.js
@@ -1,50 +1,80 @@
 const tabList = document.querySelector('[role="tablist"]');
-const tabs = tabList.querySelectorAll('[role="tab]');
-
+const tabs = tabList.querySelectorAll('[role="tab"]');
 
 tabList.addEventListener('keydown', changeTabFocus);
 
 tabs.forEach((tab) => {
     tab.addEventListener('click', changeTabPanel);
-})
+});
+
 
 let tabFocus = 0;
 function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
-
-    // change the tabindex of the current tab to -1
-    if(e.keyCode === keydownLeft || e.keyCode === keydownRight){
-        tabs[tabFocus].setAttribute("tabindex", -1);
-    }
     
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
 
-    // if the right key is pushed, move to the next tab on the right
-    if(e.keyCode === keydownRight){
-        tabFocus++;
-        if(tabFocus >= tabs.length) {
-            tabFocus = 0;
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        }else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
         }
+        
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
     }
-    // if the left key is pushed, move to the next tab on the left
-    if(e.keyCode === keydownLeft){
-        tabFocus--;
-        if(tabFocus < 0) {
-            tabFocus = tabs.length - 1;
-        }
-    }
-
-    tabs[tabFocus].setAttribute("tabindex", 0);
-    tabs[tabFocus].focus();
 }
+
 
 function changeTabPanel(e) {
     const targetTab = e.target;
     const targetPanel = targetTab.getAttribute("aria-controls");
-
+    const targetImage = targetTab.getAttribute("data-image");
+    
     const tabContainer = targetTab.parentNode;
     const mainContainer = tabContainer.parentNode;
 
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute("aria-selected", false);
 
+    targetTab.setAttribute("aria-selected", true);
+    
+    // hiding all then turning on the one that is being selected. Easier
+    // then finding previous, hiding, then turning on one selected.
+    hideContent(mainContainer, '[role="tabpanel"]');
+    showContent(mainContainer, [`#${targetPanel}`]);
+    // mainContainer
+    //     .querySelectorAll('[role="tabpanel"]')
+    //     .forEach((panel) => panel.setAttribute("hidden", true));
+    // mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+
+    // Hiding all images.
+    hideContent(mainContainer, 'picture');
+    showContent(mainContainer, [`#${targetImage}`]);
+    // mainContainer
+    //     .querySelectorAll('picture')
+    //     .forEach((pic) => pic.setAttribute("hidden", true));
+    
+    // mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
+}
+
+// Function to hide content
+function hideContent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => item.setAttribute("hidden", true));
+}
+
+// Function to show content
+function showContent(parent, content) {
+    parent.querySelector(content).removeAttribute('hidden');
 }
